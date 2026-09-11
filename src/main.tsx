34670a9b9ae2488d22b139da8a72432545ff0009
Bambang Kusniawan <153849115+imsjogja@users.noreply.github.com>
2026-09-12 01:46:30 +0700

    Add ui set B, hooks, styles (batch 6/7)

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import '@fontsource/bebas-neue'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/space-mono/400.css'
+import '@fontsource/space-mono/700.css'
+import './index.css'
+import { TRPCProvider } from "@/providers/trpc"
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <TRPCProvider>
+        <App />
+      </TRPCProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
